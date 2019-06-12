e199ea43443df826ad4e5abde1e8eea187c5ee31
Andrew <alo42@georgetown.edu>
2019-06-11 21:48:21 -0400

    remapped nod push notifications

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,19 @@ admin.initializeApp();
 
 var db = admin.firestore()
 
-function diff(newValue: Array<string>, previousValue: Array<string>): string {
-	return newValue.filter(function (i) { return previousValue.indexOf(i) < 0; })[0];
-};
-
-export const nodReceived = functions.firestore.document("/users/{userId}/nods/nodsReceived").onUpdate((change, context) => {
+export const nodReceived = functions.firestore.document("/users/{userId}/nodsReceived/{newNod}").onCreate((snap, context) => {
 	let recipientId = context.params.userId
-	let previousValue = change.before.data()
-	let newValue = change.after.data()
-	if (newValue) {
-		var newNodsReceived = newValue.nodsReceived;
-	}
-	if (previousValue) {
-		var prevNodsReceived = previousValue.nodsReceived;
-	}
-	let senderId = diff(newNodsReceived, prevNodsReceived);
-	let senderName = ""
-	let payload = {}
-	logNodPNInfo(newValue, previousValue, recipientId, senderId, newNodsReceived, prevNodsReceived);
+	let nod = snap.data()
 
-	const senderRef = db
-		.collection('users')
-		.doc(senderId);
+	let senderId = ""
+	let senderName = ""
+	if (nod) {
+		senderId = nod.senderId
+		senderName = nod.senderName
+	}
+
+	let payload = {}
+	logNodPNInfo(recipientId, senderId, nod);
 
 	let recipientRef = db
 		.collection('users')
@@ -34,8 +25,7 @@ export const nodReceived = functions.firestore.document("/users/{userId}/nods/no
 	const recipientNodsSentRef = db
 		.collection('users')
 		.doc(recipientId)
-		.collection('nods')
-		.doc('nodsSent')
+		.collection('nodsSent')
 
 	recipientRef.get().then((userSnapshot) => {
 		let fcmToken = ""
@@ -46,50 +36,45 @@ export const nodReceived = functions.firestore.document("/users/{userId}/nods/no
 			console.log(`Got FCMtoken ${fcmToken} for userId ${recipientId}`)
 		}
 
-		recipientNodsSentRef.get().then(nodsSentSnapshot => {
-			var recipientNodsSent: Array<string> = []
-			let nodsSentData = nodsSentSnapshot.data()
-			if (nodsSentData) {
-				recipientNodsSent = nodsSentData.nodsSent
+		recipientNodsSentRef.listDocuments().then(nodsSentSnapshot => {
+			var isAMutualNod: Boolean = false
+			
+			nodsSentSnapshot.forEach(doc => {
+				if (doc.id == senderId) {
+					isAMutualNod = true
+				}
+			});
+
+			if (isAMutualNod) {
+				payload = {
+					notification: {
+						title: `${senderName} nodded back at you!`,
+						body: "Start chatting!",
+						sound: "default"
+					}
+				}
+			} else {
+				payload = {
+					notification: {
+						title: `${senderName} nodded at you!`,
+						body: "Nod back?",
+						sound: "default"
+					},
+					data: {
+						senderId: senderId
+					}
+				}
 			}
 
-			senderRef.get().then((senderSnapshot) => {
-				let senderUserData = senderSnapshot.data()
-
-				if (senderUserData) {
-					senderName = senderUserData.name
-				}
-
-				if (recipientNodsSent.includes(senderId)) {
-					payload = {
-						notification: {
-							title: `${senderName} nodded back at you!`,
-							body: "Start chatting!",
-							sound: "default"
-						}
-					}
-				} else {
-					payload = {
-						notification: {
-							title: `${senderName} nodded at you!`,
-							body: "Nod back?",
-							sound: "default"
-						},
-						data: {
-							senderId: senderId
-						}
-					}
-				}
-
-				return admin.messaging().sendToDevice(fcmToken, payload)
-					.then(response => {
-						console.log("RESPONSE FROM PUSH: " + JSON.stringify(response))
-						console.log(`Nod Push Notification sent to ${fcmToken}`);
-					})
-					.catch(error => {
-						console.log("Error while sending push notification: " + JSON.stringify(error))
-					})
-			})
+			return admin.messaging().sendToDevice(fcmToken, payload)
+				.then(response => {
+					console.log("RESPONSE FROM PUSH: " + JSON.stringify(response))
+					console.log(`Nod Push Notification sent to ${fcmToken}`);
+				})
+				.catch(error => {
+					console.log("Error while sending push notification: " + JSON.stringify(error))
+				})
+			
 		})
 	})
 })
@@ -145,11 +130,8 @@ export const messageReceived = functions.firestore
 			}
 		})
 
-function logNodPNInfo(newValue: FirebaseFirestore.DocumentData | undefined, previousValue: FirebaseFirestore.DocumentData | undefined, recipientId: any, senderId: String, newNodsReceived: any, prevNodsReceived: any) {
-	console.log("NEW NODS RECEIVED: " + newNodsReceived);
-	console.log("PREVIOUS NODS RECEIVED: " + prevNodsReceived);
-	console.log("PREVIOUS VALUE: " + JSON.stringify(previousValue));
-	console.log("NEW VALUE: " + JSON.stringify(newValue));
+function logNodPNInfo(recipientId: any, senderId: String, nod: any) {
 	console.log("RECIPIENT ID: " + recipientId);
 	console.log("SENDER ID: " + senderId);
+	console.log(JSON.stringify(nod))
 }
